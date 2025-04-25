ffeaacbc3d1e92edb66732d3f946fadddb91ac83
TristanVII <tristandavis888@gmail.com>
2025-04-24 20:31:09 -0700

    view and create endpoints

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,38 @@
 class ProductsController < ApplicationController
   # this default method renders: app/views/products/index.html.erb
+  # controller methods maps to app/views/{products}/{method_name}
   def index
     # can just call Product.all here
     p = Product.new
     @products = p.all
     puts "#{@products}"
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+
+  def create
+    params = product_params
+    puts "PARAMS #{params}"
+    @product = Product.new(params)
+    if @product.save
+      redirect_to @product
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def product_params
+    # THESE TWO ARE THE SAME
+    # params.expect( product: [ :name ] )
+    params.expect({ product: [ :name ] })
   end
 end
